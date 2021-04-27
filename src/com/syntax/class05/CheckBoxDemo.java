22a26c3aa5c4523474c1c94c4e9d0209dce19946
SyntaxTechnologies <instructor@syntaxtechs.com>
2021-04-27 19:58:21 -0400

    handling checkboxes

---
+++ src/com/syntax/class05/CheckBoxDemo.java
@@ -0,0 +1,29 @@
+package com.syntax.class05;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class CheckBoxDemo {
+
+    public static String url = "http://syntaxtechs.com/selenium-practice/basic-checkbox-demo.php";
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        List<WebElement> optionCheckBoxes = driver.findElements(By.xpath("//input[@class = 'cb1-element']"));
+        System.out.println(optionCheckBoxes.size());
+        for(WebElement optionCheckBox: optionCheckBoxes) {
+            String checkBox = optionCheckBox.getAttribute("value");
+            if(checkBox.equalsIgnoreCase("Option-2")) {
+                Thread.sleep(1000);
+                optionCheckBox.click();
+                break;
+            }
+        }
+    }
+}
---
+++ src/com/syntax/class05/DropdownHandling.java
@@ -0,0 +1,21 @@
+package com.syntax.class05;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropdownHandling {
+
+    public static String url = "http://syntaxtechs.com/selenium-practice/basic-select-dropdown-demo.php";
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        WebElement daysDD = driver.findElement(By.id("select-demo"));
+        Select select = new Select(daysDD);
+        
+    }
+}
