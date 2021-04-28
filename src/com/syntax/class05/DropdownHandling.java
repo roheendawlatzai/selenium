a7aaaeb9213578b12431d8dc2378eece5656ec86
SyntaxTechnologies <instructor@syntaxtechs.com>
2021-04-27 21:56:07 -0400

    Dropdowns

--- src/com/syntax/class05/DropdownHandling.java
+++ src/com/syntax/class05/DropdownHandling.java
@@ -6,16 +6,35 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+
 public class DropdownHandling {
 
     public static String url = "http://syntaxtechs.com/selenium-practice/basic-select-dropdown-demo.php";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get(url);
         WebElement daysDD = driver.findElement(By.id("select-demo"));
         Select select = new Select(daysDD);
-        
+//        select.selectByIndex(2);
+//        Thread.sleep(1000);
+//
+//        select.selectByVisibleText("Wednesday");
+//        Thread.sleep(1000);
+//
+//        select.selectByValue("Sunday");
+
+        List<WebElement> allOptions = select.getOptions();
+        int size = allOptions.size();
+        System.out.println(size);
+
+        for(int i = 0; i < size; i++) {
+            String optionText = allOptions.get(i).getText();
+            System.out.println(optionText);
+            select.selectByIndex(i);
+            Thread.sleep(2000);
+        }
     }
 }
---
+++ src/com/syntax/class05/HandlingDDWithIterator.java
@@ -0,0 +1,34 @@
+package com.syntax.class05;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class HandlingDDWithIterator {
+
+    public static String url = "http://amazon.com";
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        WebElement categoriesDD = driver.findElement(By.id("searchDropdownBox"));
+        Select select = new Select(categoriesDD);
+        boolean isMultiple = select.isMultiple();
+        if(!isMultiple) {
+            List<WebElement> options = select.getOptions();
+            Iterator<WebElement> it = options.iterator();
+            while(it.hasNext()) {
+                WebElement ddOption = it.next();
+                String optionText = ddOption.getText();
+                System.out.println(optionText);
+            }
+        }
+    }
+}
