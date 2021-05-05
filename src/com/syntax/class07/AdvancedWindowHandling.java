cb53062aee857661c016f8a614e8a0879db7f5b5
SyntaxTechnologies <instructor@syntaxtechs.com>
2021-05-04 21:52:43 -0400

    Implicit wait demo

--- src/com/syntax/class07/AdvancedWindowHandling.java
+++ src/com/syntax/class07/AdvancedWindowHandling.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class AdvancedWindowHandling {
 
     public static String url = "http://syntaxtechs.com/selenium-practice/window-popup-modal-demo.php";
@@ -21,6 +24,20 @@ public class AdvancedWindowHandling {
         instagramButton.click();
         facebookButton.click();
         instaAndFbButton.click();
+
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        System.out.println(allWindowHandles.size());
+        Iterator<String> it = allWindowHandles.iterator();
+        while(it.hasNext()) { // start iterating through handles
+            String handle = it.next(); // get the next handle
+            if(!handle.equals(mainPageWindowHandle)) { // set a condition
+                driver.switchTo().window(handle); // switch to a window which is not equal to a parent handle
+                System.out.println(driver.getTitle());
+                driver.close();
+            }
+        }
+        driver.switchTo().window(mainPageWindowHandle); // switch back to parent
+        instagramButton.click();
     }
 
 }
